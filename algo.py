85b842a4fb6ac4048b0f1d0c760e9adac47115c6
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-03-18 14:12:16 +0100

    parsing finish

---
+++ algo.py
@@ -0,0 +1,26 @@
+from parsing import Parsing
+import sys
+from parsing import Parsing
+
+class Djikstra:
+    def __init__(self) -> None:
+        self.parser = Parsing()
+        self.start = None
+        self.end = None
+        self.path = None
+
+    def parse_file(self, file: str):
+        self.parser.read_file(file)
+        self.parser.check_line()
+        self.parser.parse()
+        self.start = self.parser.start
+        self.end = self.parser.end
+        self.path = self.parser.zones
+
+if __name__ == "__main__":
+    try:
+        test = Djikstra()
+        test.parse_file(sys.argv[len(sys.argv) - 1])
+        print(test.parser.zones)
+    except Exception as e:
+        print(e)
--- parsing.py
+++ parsing.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Any
 from dataclasses import dataclass
 from enum import Enum
 
@@ -12,6 +12,8 @@ class Parsing:
         self.zones: List = []
         self.connections: List = []
         self.pos = []
+        self.start = []
+        self.end = []
 
     def read_file(self, file: str) -> None:
         with open(file, "r") as fd:
@@ -29,15 +31,41 @@ class Parsing:
     def parse_nb_drones(self, line: str) -> None:
         res = line.split(":")
         self.nb_drones = int(res[1].strip())
+        if self.nb_drones < 1:
+            raise ValueError("nb of drone can't be smaller than 1")
 
     def parse_zone(self, line: str) -> None:
         parts = line.split()
         name = parts[1]
         x = int(parts[2])
         y = int(parts[3])
-        color = parts[4]
+        color = "white"
+        if name == "start":
+            self.start.append((x, y))
+        if name == "goal":
+            self.end.append((x, y))
+        if "[" in line:
+            pars = line.split("[")[1].split("]")[0]
+            tags = pars.split()
+            zone_pars = None
+            for tag in tags:
+                if "=" in tag:
+                    key, value = tag.split("=", 1)
+                    if key == "color":
+                        color = value
+                    if key == "zone":
+                        zone_pars = value
+                    if key == "max_drones":
+                        max_drone = int(value)
         self.pos.append((x, y, color))
-        self.zones.append((name, x, y, color))
+        if zone_pars is not None and max_drone is not None:
+            self.zones.append((name, x, y, color, zone_pars, max_drone))
+        elif zone_pars is None and max_drone is not None:
+            self.zones.append((name, x, y, color, max_drone))
+        elif zone_pars is not None and max_drone is None:
+            self.zones.append((name, x, y, color, zone_pars))
+        else:
+            self.zones.append((name, x, y, color))
 
     def parse(self) -> None:
         for line in self.all_line:
@@ -56,15 +84,7 @@ class Parsing:
         left = line.split("[")[0]
         part = left.split()[1]
         zone1, zone2 = part.split("-")
-        capacity = 1
-        if "[" in line:
-            metadata = line.split("[")[1].rstrip("]")
-            tags = metadata.split()
-            for tag in tags:
-                key, value = tag.split("=")
-                if key == "max_link_capacity":
-                    capacity = int(value)
-        self.connections.append((zone1, zone2, capacity))
+        self.connections.append((zone1, zone2))
 
 
 class ZoneType(Enum):
