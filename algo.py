66f25ddb596cd506026b91738e3c7ab756a8d412
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-03-21 10:57:45 +0100

    algo for one drone finish

--- algo.py
+++ algo.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from collections import deque
 
 
-class Djikstra:
+class BFS:
     def __init__(self) -> None:
         self.parser = Parsing()
         self.start: List[tuple[str, int, int]] = []
@@ -19,22 +19,46 @@ class Djikstra:
         self.end = self.parser.end
         self.connection = self.parser.connections
 
+    def build_adj(self):
+        adj = {}
+        for zone in self.parser.zones:
+            adj[zone['name']] = []
+        for z1, z2 in self.parser.connections:
+            name1 = z1['name']
+            nmae2 = z2['name']
+            adj[name1].append(z2)
+            adj[nmae2].append(z1)
+        return adj
+
     def get_path(self) -> None:
-        visite = []
-        parent = []
-        queue = deque(self.start)
+        start_node = self.start[0][0]
+        end_node = self.end[0][0]
+        queue = deque([start_node])
+        visited = {start_node}
+        parent = {}
         while queue:
             current = queue.popleft()
-            visite.append(current)
-            if current == self.end:
+            if current == end_node:
                 break
-            direction = deque()
-            for i in self.parser.connections:
-                if i[0]['name'] == current[0]:
-                    if i[0].get('zone') != "blocked":
-                        direction.append(i[1])
-                directions =sorted(direction, key=lambda x: x['zone'] == 'priority')
-        print(direction)
+            voisin = self.build_adj()[current]
+            for v in voisin:
+                name_v = v['name']
+                if name_v not in visited:
+                    if v.get('zone') != "blocked":
+                        visited.add(name_v)
+                        parent[name_v] = current
+                        queue.append(name_v)
+        if end_node not in parent and start_node != end_node:
+            print("ERROR")
+            return
+        path = []
+        curr = end_node
+        while curr is not None:
+            path.append(curr)
+            curr = parent.get(curr)
+        self.path = path[::-1]
+        print(f"chemin : {self.path}")
+
             
             
 
@@ -43,7 +67,7 @@ class Djikstra:
 
 if __name__ == "__main__":
     try:
-        test = Djikstra()
+        test = BFS()
         test.parse_file(sys.argv[len(sys.argv) - 1])
         test.get_path()
     except Exception as e:
